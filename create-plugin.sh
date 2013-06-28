328f7d3b779ea28df1671a8433beafd40146915e
Joel Goguen <jgoguen@jgoguen.ca>
2013-06-27 21:31:43 -0300

    Add the 'sql' folder to the package

--- create-plugin.sh
+++ create-plugin.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip -u KoboTouchExtended.zip container.py hyphenator.py __init__.py driver.py plugin-import-name-kobotouch_extended.txt 
+zip -ru KoboTouchExtended.zip container.py hyphenator.py __init__.py driver.py plugin-import-name-kobotouch_extended.txt sql
