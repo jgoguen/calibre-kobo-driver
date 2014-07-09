d565648e132ba734527c759a8d229191ea63b8e0
Joel Goguen <jgoguen@fb.com>
2014-07-09 15:34:39 -0700

    Update to work with calibre 2 PyQt5

--- conversion/config.py
+++ conversion/config.py
@@ -38,8 +38,12 @@ class PluginWidget(EPUBPluginWidget, EPUBUIForm):
     def setupUi(self, Form):
         super(PluginWidget, self).setupUi(Form)
 
-        from PyQt4 import QtCore
-        from PyQt4 import QtGui
+		try:
+	        from PyQt4 import QtCore
+		    from PyQt4 import QtGui
+		except ImportError:
+			from PyQt5 import Qt as QtGui
+			from PyQt5 import QtCore
         from calibre.gui2.convert.epub_output_ui import _fromUtf8
 
         rows = self.gridLayout.rowCount() - 1
